da7e683453027c27ff591151a0b60d398a131016
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-10-03 13:49:15 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import AppMap from './components/AppMap';
 import Landing from './components/Landing';
 import Header from './components/Header';
-import './bootstrap.css';
+import Cards from './components/Cards';
 
 function App() {
   return (
