6706996b77e293f7cd00068cef57dbafcefc4f9f
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-10-03 11:02:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel'
 
 function App() {
   return (
