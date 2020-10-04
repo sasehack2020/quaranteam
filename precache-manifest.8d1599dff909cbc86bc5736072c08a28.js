6d819581a81f7651af985cd5d9fa92cfd0788070
Your Name <nathanielbd@gmail.com>
2020-10-03 21:29:53 -0500

    Updates

---
+++ precache-manifest.8d1599dff909cbc86bc5736072c08a28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e3ab9da7ebaef2a992088d58780ff4",
+    "url": "/quaranteam/index.html"
+  },
+  {
+    "revision": "63e9df8000e276084b49",
+    "url": "/quaranteam/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "bc758c3328e5aba6b294",
+    "url": "/quaranteam/static/css/main.9b89ee53.chunk.css"
+  },
+  {
+    "revision": "63e9df8000e276084b49",
+    "url": "/quaranteam/static/js/2.987e284b.chunk.js"
+  },
+  {
+    "revision": "17dffa516c2ca25cecdb67ecbc5e1c6e",
+    "url": "/quaranteam/static/js/2.987e284b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc758c3328e5aba6b294",
+    "url": "/quaranteam/static/js/main.4ae9cb10.chunk.js"
+  },
+  {
+    "revision": "2c09683776a485e844ec",
+    "url": "/quaranteam/static/js/runtime-main.a3284f21.js"
+  }
+]);
